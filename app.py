952bc7ccfdcdf3769173598626e6352011250f95
gerardo agustin <gerardoagustin@hotmail.com>
2019-03-15 13:05:15 -0300

    Login y Registro listo falta testear

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flaskext.mysql import MySQL
+# linea nueva
+from flask_sqlalchemy import SQLAlchemy
+#
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 mysql = MySQL()
 
 # MySQL Connection
@@ -11,16 +15,63 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'bd_app'
 mysql.init_app(app)
 
+# Alchemy Connection
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Gerardo/Desktop/programa BIT/proyecto final/Codigo proyecto final/login_app/database.db'
+
+
+
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(80))
+# Termina Alchemy Connection
+
+
 # Settings
 app.secret_key = 'mysecretkey'
 
 
-@app.route('/')
+
+# ##################################### R U T A S  D E  L O G I N  Y  R E G I S T R O #######################
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        uname = request.form["uname"]
+        passw = request.form["passw"]
+
+        login = user.query.filter_by(username=uname, password=passw).first()
+        if login is not None:
+            return redirect(url_for("menu_principal"))
+    return render_template("login.html")
+
+
+@app.route("/menu_principal")
 def menu_principal():
     return render_template("menu_principal.html")
 
 
-# ##################################### E Q U I P O S / M A N T E N I M I E N T O ################################################
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        uname = request.form['uname']
+        mail = request.form['mail']
+        passw = request.form['passw']
+
+        register = user(username=uname, email=mail, password=passw)
+        db.session.add(register)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("register.html")
+
+
+# ##################################### R U T A S  D E  E Q U I P O S / M A N T E N I M I E N T O ################################################
 @app.route('/equipos')
 def Index_equipos():
     conn = mysql.connect()
@@ -92,7 +143,7 @@ def delete_equipment(id):
     flash('Equipment Removed Successfully')
     return redirect(url_for('Index_equipos'))
 
-# ##################################### E N E R G E T I C O S ################################################
+# ##################################### R U T A S  D E  E N E R G E T I C O S ################################################
 @app.route('/energy')
 def Index_energy():
     conn = mysql.connect()
